1900f05074d061eab4747ccf354bf53189fa1cba
khoagrai <khoagrai@gmail.com>
2026-05-08 14:13:12 +0700

    fixed-commit

--- CO3094-asynaprous/start_sampleapp.py
+++ CO3094-asynaprous/start_sampleapp.py
@@ -1,46 +1,25 @@
-#
-# Copyright (C) 2026 pdnguyen of HCMC University of Technology VNU-HCM.
-# All rights reserved.
-# This file is part of the CO3093/CO3094 course,
-# and is released under the "MIT License Agreement". Please see the LICENSE
-# file that should have been included as part of this package.
-#
-# AsynapRous release
-#
-# The authors hereby grant to Licensee personal permission to use
-# and modify the Licensed Source Code for the sole purpose of studying
-# while attending the course
-#
+"""Entry point for the AsynapRous sample chat web application."""
 
-
-"""
-start_sampleapp
-~~~~~~~~~~~~~~~~~
-
-This module provides a sample RESTful web application using the AsynapRous framework.
-
-It defines basic route handlers and launches a TCP-based backend server to serve
-HTTP requests. The application includes a login endpoint and a greeting endpoint,
-and can be configured via command-line arguments.
-"""
-
-import json
-import socket
 import argparse
+import os
 
 from apps import create_sampleapp
 
-PORT = 2026  # Default port
+PORT = 2026
+
 
 if __name__ == "__main__":
-    # Parse command-line arguments to configure server IP and port
-    parser = argparse.ArgumentParser(prog='Backend', description='', epilog='Beckend daemon')
-    parser.add_argument('--server-ip', default='0.0.0.0')
-    parser.add_argument('--server-port', type=int, default=PORT)
- 
+    parser = argparse.ArgumentParser(prog="SampleApp", epilog="AsynapRous app")
+    parser.add_argument("--server-ip", default="0.0.0.0")
+    parser.add_argument("--server-port", type=int, default=PORT)
+    parser.add_argument(
+        "--async-mode",
+        choices=["threading", "callback", "coroutine"],
+        default=None,
+        help="Concurrency mode. Overrides ASYNC_MODE environment variable.",
+    )
     args = parser.parse_args()
-    ip = args.server_ip
-    port = args.server_port
 
-    # Prepare and launch the RESTful application
-    create_sampleapp(ip, port)
+    if args.async_mode:
+        os.environ["ASYNC_MODE"] = args.async_mode
+    create_sampleapp(args.server_ip, args.server_port)
